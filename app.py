5664e9bacdff3ff3b4bb017a7ac44c8db117b4ee
markdoge <63504347+markdoge@users.noreply.github.com>
2022-08-04 13:04:57 +0800

    markdoge
    推荐下工资饼图

--- app.py
+++ app.py
@@ -1,33 +1,38 @@
-from flask import Flask,render_template,redirect,request,session
+from flask import Flask, render_template, redirect, request, session
 from service.UserService import UserService
 from service.JobService import JobService
 from controller.UserController import userController
 from controller.JobController import jobController
+
 app = Flask(__name__)
-app.debug=True
-app.config['SECRET_KEY']="AFSGGSFAFAS"
+app.debug = True
+app.config['SECRET_KEY'] = "AFSGGSFAFAS"
 app.register_blueprint(userController)
 app.register_blueprint(jobController)
+
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/login',methods=['post'])
+
+@app.route('/login', methods=['post'])
 def login():
     userService = UserService()
-    userName=request.form['userName']
-    password=request.form['password']
+    userName = request.form['userName']
+    password = request.form['password']
     try:
-        user=userService.getUserByUserName(userName)
-        if user and user['password']==password:
-            session["user"]=user
-            jobService=JobService()
-            jobData=jobService.getTopSalaryJobs()
-            return render_template('main.html', userName=userName,jobData=jobData)
+        user = userService.getUserByUserName(userName)
+        if user and user['password'] == password:
+            session["user"] = user
+            jobService = JobService()
+            jobData = jobService.getTopSalaryJobs()
+            return render_template('main.html', userName=userName, jobData=jobData)
         else:
             return render_template('index.html', message="密码错误")
     except Exception as e:
         print(e)
+
 
 # 退出
 @app.route("/logout")
@@ -38,17 +43,20 @@ def logout():
     return render_template("index.html")
     pass
 
+
 @app.route("/main")
 def toMain():
-    jobService=JobService()
-    jobData=jobService.getTopSalaryJobs()
-    return render_template("main.html",jobData=jobData)
+    jobService = JobService()
+    jobData = jobService.getTopSalaryJobs()
+    return render_template("main.html", jobData=jobData)
     pass
+
 
 @app.route("/test")
 def ajaxTest():
     return render_template("ajaxTest.html")
     pass
+
 
 if __name__ == '__main__':
     app.run()
--- controller/JobController.py
+++ controller/JobController.py
@@ -1,136 +1,146 @@
-from flask import render_template,redirect,request,sessions,Blueprint
+from flask import render_template, redirect, request, sessions, Blueprint
 import json
 from service.JobService import JobService
 from service.UserService import UserService
 
-jobController=Blueprint("jobController",__name__)
+jobController = Blueprint("jobController", __name__)
 
-@jobController.route('/getjobsalarybytype',methods=['get','post'])
+
+@jobController.route('/getjobsalarybytype', methods=['get', 'post'])
 def getJobSalaryByJobType():
-    jobService=JobService()
-    data=jobService.getJobSalaryByJobType()
-    return json.dumps(data,ensure_ascii=False)
+    jobService = JobService()
+    data = jobService.getJobSalaryByJobType()
+    return json.dumps(data, ensure_ascii=False)
     pass
 
-@jobController.route('/getjobcountbytype',methods=['get','post'])
+
+@jobController.route('/getjobcountbytype', methods=['get', 'post'])
 def getJobCountByJobType():
-    jobService=JobService()
-    data=jobService.getJobCountByJobType()
-    return json.dumps(data,ensure_ascii=False)
+    jobService = JobService()
+    data = jobService.getJobCountByJobType()
+    return json.dumps(data, ensure_ascii=False)
     pass
 
-@jobController.route('/getjobcountbycity',methods=['get','post'])
+
+@jobController.route('/getjobcountbycity', methods=['get', 'post'])
 def getJobCountByJobCity():
-    jobService=JobService()
-    data=jobService.getJobCountByJobCity()
-    return json.dumps(data,ensure_ascii=False)
+    jobService = JobService()
+    data = jobService.getJobCountByJobCity()
+    return json.dumps(data, ensure_ascii=False)
     pass
 
-@jobController.route('/getjobhlsalarybytype',methods=['get','post'])
+
+@jobController.route('/getjobhlsalarybytype', methods=['get', 'post'])
 def getJobHLSalaryByJobCity():
-    jobService=JobService()
-    data=jobService.getJobHLSalaryByJobType()
-    return json.dumps(data,ensure_ascii=False)
+    jobService = JobService()
+    data = jobService.getJobHLSalaryByJobType()
+    return json.dumps(data, ensure_ascii=False)
     pass
 
-@jobController.route('/joblist',methods=['post','get'])
+
+@jobController.route('/joblist', methods=['post', 'get'])
 def joblist():
-    searchName=request.form.get("searchName")
-    currentPage=request.form.get("currentPage")
-    pageSize=request.form.get("pageSize")
-    opr=request.form.get("opr")
-    jobID=request.form.get("jobID")
+    searchName = request.form.get("searchName")
+    currentPage = request.form.get("currentPage")
+    pageSize = request.form.get("pageSize")
+    opr = request.form.get("opr")
+    jobID = request.form.get("jobID")
 
-    currentPage=1 if not currentPage else int(currentPage)
-    pageSize=10 if not pageSize else int(pageSize)
-    startRow=(currentPage-1)*pageSize
-    page={"currentPage":currentPage,"pageSize":pageSize,"startRow":startRow}
+    currentPage = 1 if not currentPage else int(currentPage)
+    pageSize = 10 if not pageSize else int(pageSize)
+    startRow = (currentPage - 1) * pageSize
+    page = {"currentPage": currentPage, "pageSize": pageSize, "startRow": startRow}
 
-    search={}
+    search = {}
     if searchName:
-        search={"jobName":searchName}
-    searchType=request.form.getlist("jobTypeSelect")
+        search = {"jobName": searchName}
+    searchType = request.form.getlist("jobTypeSelect")
     searchCity = request.form.getlist("jobCitySelect")
-    searchLowSalary=request.form.get("searchLowSalary")
+    searchLowSalary = request.form.get("searchLowSalary")
     searchHighSalary = request.form.get("searchHighSalary")
     if searchType:
         if "全部" in searchType:
-            searchType=None
+            searchType = None
         else:
-            search["jobType"]=searchType
-            page["selectType"]=searchType
+            search["jobType"] = searchType
+            page["selectType"] = searchType
     if searchCity:
         if "全部" in searchCity:
-            searchCity=None
+            searchCity = None
         else:
-            search["jobCity"]=searchCity
+            search["jobCity"] = searchCity
             page["selectCity"] = searchCity
     if searchLowSalary:
-        search["searchLowSalary"]=searchLowSalary
+        search["searchLowSalary"] = searchLowSalary
         page["searchLowSalary"] = searchLowSalary
     if searchHighSalary:
-        search["searchHighSalary"]=searchHighSalary
+        search["searchHighSalary"] = searchHighSalary
         page["searchHighSalary"] = searchHighSalary
-    jobService=JobService()
-    result=0
-    if opr and opr=="del":
-        result=jobService.removeJob(jobID)
+    jobService = JobService()
+    result = 0
+    if opr and opr == "del":
+        result = jobService.removeJob(jobID)
         pass
-    elif opr and opr=="update":
-        jobLowSalary=request.form.get("jobLowSalary")
-        jobHighSalary=request.form.get("jobHighSalary")
-        jobAverageSalary=(float(jobLowSalary)+float(jobHighSalary))/2.0
-        data={"jobID":jobID,"jobLowSalary":jobLowSalary,"jobHighSalary":jobHighSalary,"jobAverageSalary":jobAverageSalary}
-        result =jobService.updateJob(data)
-    elif opr and opr=="add":
+    elif opr and opr == "update":
+        jobLowSalary = request.form.get("jobLowSalary")
+        jobHighSalary = request.form.get("jobHighSalary")
+        jobAverageSalary = (float(jobLowSalary) + float(jobHighSalary)) / 2.0
+        data = {"jobID": jobID, "jobLowSalary": jobLowSalary, "jobHighSalary": jobHighSalary,
+                "jobAverageSalary": jobAverageSalary}
+        result = jobService.updateJob(data)
+    elif opr and opr == "add":
         jobCompany = request.form.get("jobCompany")
-        jobName= request.form.get("jobName")
-        jobCity= request.form.get("jobCity")
+        jobName = request.form.get("jobName")
+        jobCity = request.form.get("jobCity")
         jobType = request.form.get("jobType")
         jobLowSalary = request.form.get("jobLowSalary")
         jobHighSalary = request.form.get("jobHighSalary")
         jobAverageSalary = (float(jobLowSalary) + float(jobHighSalary)) / 2.0
 
         data = {"jobID": jobID, "jobLowSalary": jobLowSalary, "jobHighSalary": jobHighSalary,
-                "jobAverageSalary": jobAverageSalary,"jobCompany":jobCompany,"jobName":jobName,"jobCity":jobCity,"jobType":jobType}
-        result =jobService.addJob(data)
+                "jobAverageSalary": jobAverageSalary, "jobCompany": jobCompany, "jobName": jobName, "jobCity": jobCity,
+                "jobType": jobType}
+        result = jobService.addJob(data)
 
-    pageList,count=jobService.getJobPageList(search,page)
-    allType,allCity=jobService.getAllTypeAndCity(search)
-    page['pageList']=pageList
-    page['count']=count
-    totalPage=(count//pageSize+(1 if count%pageSize else 0))
-    page['totalPage']=totalPage
-    page['allType']=allType
-    page['allCity']=allCity
-    return render_template("joblist.html",page=page,search=search,result=result)
+    pageList, count = jobService.getJobPageList(search, page)
+    allType, allCity = jobService.getAllTypeAndCity(search)
+    page['pageList'] = pageList
+    page['count'] = count
+    totalPage = (count // pageSize + (1 if count % pageSize else 0))
+    page['totalPage'] = totalPage
+    page['allType'] = allType
+    page['allCity'] = allCity
+    return render_template("joblist.html", page=page, search=search, result=result)
+
 
 import requests
 from lxml import etree
 import time
-@jobController.route("/scapyjobdetail",methods=['post','get'])
+
+
+@jobController.route("/scapyjobdetail", methods=['post', 'get'])
 def scrapyJobDetail():
     requests.adapters.DEFAULT_RETRIES = 5  # 增加重连次数
     search = {}
-    currentPage=1
-    pageSize=50
-    startRow=0
+    currentPage = 1
+    pageSize = 50
+    startRow = 0
     page = {'currentPage': currentPage,
             'pageSize': pageSize,
             'startRow': startRow}
     jobService = JobService()
     pageList, count = jobService.getJobPageList(search, page)
-    totalCount=jobService.getCount()#总条目数
-    while(pageList):
+    totalCount = jobService.getCount()  # 总条目数
+    while (pageList):
 
         print(page)
         for row in pageList:
             url = row.get('jobLink')
-            detail=row.get("jobDetail")
-            if detail!=None:
+            detail = row.get("jobDetail")
+            if detail != None:
                 continue
-            header = {"Host": "www.liepin.com",'Connection': 'close',
-                      "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.134 Safari/537.36 Edg/103.0.1264.77",}
+            header = {"Host": "www.liepin.com", 'Connection': 'close',
+                      "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.134 Safari/537.36 Edg/103.0.1264.77", }
             print(row.get("jobID"))
             data = {"statusName": "scrapy", "now": row.get("jobID"), "counts": totalCount}
             jobService.updateJobStatus(data)
@@ -142,29 +152,30 @@ def scrapyJobDetail():
                 if item:
                     content = item[0]
 
-                    data={"jobID":row.get("jobID"),"jobDetail":content}
+                    data = {"jobID": row.get("jobID"), "jobDetail": content}
                     jobService.updateJobDetail(data)
                     pass
             time.sleep(3)
             pass
-        currentPage+=1
-        startRow=(currentPage-1)*pageSize
+        currentPage += 1
+        startRow = (currentPage - 1) * pageSize
         page = {'currentPage': currentPage,
                 'pageSize': pageSize,
                 'startRow': startRow}
         pageList, count = jobService.getJobPageList(search, page)
     pass
-    return json.dumps({"result":"finish"},ensure_ascii=False)
+    return json.dumps({"result": "finish"}, ensure_ascii=False)
 
 
 import jieba
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 from sklearn.metrics.pairwise import linear_kernel
 import numpy as np
-@jobController.route("/jobsimilar",methods=['post','get'])
-def jobSimilar():
 
-    jobService  = JobService()
+
+@jobController.route("/jobsimilar", methods=['post', 'get'])
+def jobSimilar():
+    jobService = JobService()
     jobList = jobService.getAllJobList()
     texts = []
     for row in jobList:
@@ -183,7 +194,7 @@ def jobSimilar():
         job = jobList[i]
         cosine_similarities = linear_kernel(tfiwf[i], tfiwf).flatten()
         top10List = []
-        scores=[]
+        scores = []
 
         for t in range(11):
             index = np.argmax(cosine_similarities)
@@ -192,9 +203,9 @@ def jobSimilar():
             cosine_similarities[index] = -1
             pass
         # 写入数据库
-        for row,score in zip(top10List,scores):
+        for row, score in zip(top10List, scores):
             if row.get('jobID') != job.get('jobID'):
-                data = {'jobId': job.get('jobID'), 'similarJobId': row.get('jobID') ,'score':score}
+                data = {'jobId': job.get('jobID'), 'similarJobId': row.get('jobID'), 'score': score}
                 jobService.addSimilarJob(data)
             pass
         data = {"statusName": "similarJob", "now": i, "counts": len(jobList)}
@@ -202,17 +213,27 @@ def jobSimilar():
     return json.dumps({"result": "finish"}, ensure_ascii=False)
     pass
 
-@jobController.route("/jobdetail",methods=['post','get'])
+
+@jobController.route("/jobdetail", methods=['post', 'get'])
 def getJobDetail():
     jobId = request.args.get("jobID")
     jobService = JobService()
     job, sjobList = jobService.getJobDetails(jobId)
-
+    job_list = list()
+    for jobs in sjobList:
+        temp = dict()
+        temp["jobCompany"] = jobs["jobCompany"]
+        char = int(jobs["jobLowSalary"]) / 1000
+        temp["jobLowSalary"] = char
+        job_list.append(temp)
+    filename = './/static/assets/js/job_similar.json'
+    jsonfile = open(filename, 'w', encoding='utf-8')
+    json.dump(job_list, jsonfile, ensure_ascii=False, indent=4)
     return render_template("jobdetail.html", job=job, sjobList=sjobList)
     pass
 
 
-@jobController.route("/jobstatus",methods=['post','get'])
+@jobController.route("/jobstatus", methods=['post', 'get'])
 def getJobStatus():
     statusName = request.form.get("statusName")
     jobService = JobService()
